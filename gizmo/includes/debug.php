3473bd68a5bea4eded0d1d56f6566b09d82579a5
Alexander Whillas <whillas@gmail.com>
2010-09-23 17:47:37 +0200

    Doco for the query language. Added some handy PHP debug functions

---
+++ gizmo/includes/debug.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Handy Debugging functions
+ *
+ * @package WebGizmo
+ * @author Alexander Whillas
+ **/
+
+/**
+ * shortcut for print_r
+ *
+ * @return String
+ **/
+function pr($subject)
+{
+	echo "\n".print_r($subject, true)."\n";
+}
+
+/**
+ * shortcut for var_dump()
+ *
+ * @return String
+ **/
+function vd($subject)
+{
+	var_dump($subject);
+}
+
+/**
+ * Stack Dump
+ *
+ * @return String
+ **/
+function sd($html = true)
+{
+	if($html)
+		pr(debug_backtrace());
+	else
+		krumo(debug_backtrace());
+}
+
+/**
+ * Print what type of subject is passed
+ *
+ * @return String
+ **/
+function what($subject)
+{
+	switch(true)
+	{
+		case is_array($subject):
+			echo 'Array';
+		
+		case is_object($subject):
+			echo get_class($subject);
+	}
+}
