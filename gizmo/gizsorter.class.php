9b27d89bc43f67e3f4c385fd42e1dd73cbaea5f8
Alexander Whillas <whillas@gmail.com>
2010-10-13 19:58:15 +0200

    Added basic sorting to the query language

--- gizmo/gizsorter.class.php
+++ gizmo/gizsorter.class.php
@@ -8,19 +8,141 @@
 /**
  * Contains various File (FSObject) array sorting commands
  *
- * @see GizCommand, GizFilter
+ * @see GizCommand, GizFilter, GizQuery::run()
  * @package WebGizmo
+ * @todo Sort by Date
+ * @todo Sort by Reverse
  **/
 class GizSorter extends GizCommand
 {
 	public static function go($sort, $subject, $param)
 	{
-		return $subject;
+		// General array sorting, not looking at its objects.
+		switch(strtolower($sort))
+		{
+			case 'shuffle':
+			case 'random':
+			case 'rand':
+				return $this->shuffle($subject);
+
+			case 'reverse':
+			case 'rev':
+				return array_reverse($subject, TRUE);
+		}		
+		
+		// some alias'
+		switch(strtolower($sort))
+		{
+			case 'nicename':
+				$sort = 'name';
+				break;
+
+			case 'rname':
+				$sort = 'realname';
+				break;
+
+			case 'filesize':
+				$sort = 'size';
+				break;
+		}
+		
+		usort($subject, array('GizSorter', $sort));
 	}
 	
+	/**
+	 * @return 	A list of available filters in an array.
+	 */
 	public static function getList()
 	{
-		return array();
+		return array(
+			'name', 'nicename', 'realname', 'rname', 'size', 'filesize', 'ctime', 'mtime',
+			'shuffle', 'random', 'rand', 'reverse', 'rev'
+		);
+	}
+
+	/**
+	 * undocumented function
+	 *
+	 * @return void
+	 * @author Alexander Whillas
+	 **/
+	function shuffle($list) 
+	{ 
+		$keys = array_keys($list);
+		
+		shuffle($keys); 
+		// $list gets sorted in the same way that $keys does
+		array_multisort($keys, $list);
+		
+		return $list; 
+	} 
+	
+	/**
+	 * Compare "nice" names.
+	 * Use realname (rname) for actual file name. 
+	 *
+	 * @return Integer
+	 * @author Alexander Whillas
+	 **/
+	public function name($a, $b)
+	{
+		return strcmp($a->getCleanName(), $b->getCleanName());
+	}
+
+	/**
+	 * Compare real file names
+	 *
+	 * @return Integer
+	 **/
+	public function realname($a, $b)
+	{
+		return strcmp($a->getFilename(), $b->getFilename());
+	}
+	
+	/**
+	 * Compare file sizes
+	 *
+	 * @return Integer	
+	 **/
+	public function size($a, $b)
+	{
+		return $this->intcmp((int)$a->getSize(), (int)$b->getSize());
+	}
+	
+	/**
+	 * Compare creation times
+	 *
+	 * @return void
+	 * @author Alexander Whillas
+	 **/
+	public function ctime($a, $b)
+	{
+		return $this->intcmp($a->getMTime(), $b->getMTime());
+	}
+
+	/**
+	 * Compare modified times
+	 *
+	 * @return void
+	 * @author Alexander Whillas
+	 **/
+	public function mtime($a, $b)
+	{
+		return $this->intcmp($a->getMTime(), $b->getMTime());
+	}	
+	
+	/**
+	 * Compare two integers.
+	 * Used on a few compare functions.
+	 *
+	 * @return Integer
+	 **/
+	public function intcmp($a, $b)
+	{
+		if ($a == $b) 
+			return 0;
+		
+		return ($a > $b) ? +1 : -1;
 	}
 	
 } // END class GizSorter
