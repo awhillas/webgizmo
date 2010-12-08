8b47cd07e89b7f80c3908dd3db806c07dae196b0
Alexander Whillas <whillas@gmail.com>
2010-12-08 21:02:55 +0100

    Comments to the /giz default template. Lead by example

--- templates/html/giz/index.tpl.php
+++ templates/html/giz/index.tpl.php
@@ -7,35 +7,45 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=<?php echo CHAR_ENCODING ?>">
 	<meta name="generator" content="Web Gizmo <?php echo $gizmo_version ?>">
 	
+	<!-- YUI online  -->
 	<link rel="stylesheet" href="http://yui.yahooapis.com/2.7.0/build/reset-fonts-grids/reset-fonts-grids.css" type="text/css">
 	<link rel="stylesheet" href="http://yui.yahooapis.com/2.7.0/build/base/base.css" type="text/css">
 	
+	<!-- Local laptop verson for workin offline -->
 	<link rel="stylesheet" href="http://yui.dev/build/reset-fonts-grids/reset-fonts-grids.css" type="text/css">
 	<link rel="stylesheet" href="http://yui.dev/build/base/base.css" type="text/css">
 	
+	<!-- Shared CSS file. $templates points to the URI path of the current template  -->
 	<link rel="stylesheet" href="<?php echo $templates ?>/style.css" type="text/css">	
 	
-	<!-- Fonts -->
+	<!-- Fonts. TODO: auto font detect and include. -->
 	<link href="http://kernest.com/fonts/komika-axis.css" media="screen" rel="stylesheet" type="text/css" />
 	<link href="http://fonts.googleapis.com/css?family=Droid+Sans&amp;subset=latin" rel="stylesheet" type="text/css">
 	
 	<?php echo $head ?>
 	
 </head>
-<body>
+<!-- Some classes based on path to make page specific styling easier.  -->
+<body class="<?php echo $fs->pathCSS() ?>">
 <div id="doc">
 	
 	<div id="hd" role="banner">
-		<?php echo ul($fs->menu(), 'Menu') ?>
+		
 		<h1><a href="<?php echo $home ?>"><?php echo $title ?></a></h1>
+		
+		<!-- Menu's are easy. $fs is the global "File System" object which makes the folder links.  -->
+		<!-- ul() is part of the simple HTML markup lib with Gizmo with functions names after tags.  -->
+		<?php echo ul($fs->menu(), 'Menu') ?>
+
 	</div>
 	
 	<div id="bd" role="main">
 		
 		<div class="yui-b LeadingBlurb">
 
-			<!-- YOUR DATA GOES HERE -->
-			
+			<!-- Content (files/folders) tagged with top (i.e. file name starts with '_top_')  -->
+			<!-- will get inserted here. The @ hides errors when there is nothing with tag in  -->
+			<!-- the current path.  -->
 			<?php echo @$top ?>
 			
 		</div>
@@ -59,8 +69,7 @@
 
 		<div class="yui-b">
 
-			<!-- YOUR DATA GOES HERE -->
-			
+			<!-- Anything thats not tagged and doesn't start with an underscore ends up here. -->
 			<?php echo $content ?>
 			
 		</div>
@@ -70,8 +79,7 @@
 	<div id="ft" role="contentinfo">
 		<hr />
 		<?php echo $foot ?>
-		<p>Powered by <a href="http://gizmo.tsd.net.au">WebGizmo</a> and <a href="http://kernest.com">Fonts via Kernest</a></p>
-		
+		<p>Powered by <a href="http://gizmo.tsd.net.au">Gizmo</a></p>
 	</div>
 
 </div>
