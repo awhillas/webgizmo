a7cec245cd9a84dc59fdc1cc9564bf23b5fdc9c9
Alexander Whillas <whillas@gmail.com>
2011-03-17 22:27:58 +0100

    Added new Twitter class for twitter feeds. MOved the handler map to gizmo folder

---
+++ gizmo/plugins/twitter.class.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @package WebGizmo
+ * @author Alexander R B Whillas
+ * @license http://www.gnu.org/copyleft/lesser.html LGPL
+ **/
+
+/**
+ * Remy sharp's
+ * 
+ * @link http://code.google.com/p/twitterjs/
+ * 
+ * @package WebGizmo
+ * @subpackage	GizPlugins
+ * @author Alexander R B Whillas
+ */
+class Twitter extends FSDir
+{
+	function html()
+	{
+		$out = '';
+		
+		$id = $this->getID();
+		
+		$fs = FS::get();
+		
+		$fs->addRef('http://twitterjs.googlecode.com/svn/trunk/src/twitter.min.js');
+		
+		$fs->add('
+		<script type="text/javascript" charset="utf-8">
+			getTwitters("'.$id.'", { id: "'.$this->getCleanName().'", count: 1 });
+		</script>		
+		');
+
+		return div($out, 'Twitter', $id);
+	}
+}
