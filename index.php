293667723eea83e3f673480e5a531ab629166149
Alexander Whillas <whillas@gmail.com>
2011-05-20 14:58:30 +0200

    Minor changes

--- index.php
+++ index.php
@@ -9,8 +9,11 @@
 
 	// Change these variables per website
 	define('SITE_TITLE', 	'Web Gizmo');	// Name of the website.
-	define('THEME_DIR', 	'/giz');		// Template top use. i.e. /templates/html/giz
+	// define('THEME_DIR', 	'/giz');		// Template top use. i.e. /templates/html/giz
+	define('THEME_DIR', 	'/less');
 	define('FOLDER_DISPLAY', 'links');	// How folders are handled. Can be 'links', 'teaser' or 'none'
+	
+	define('DEBUG', false);	// set to True to see errors.
 
 	// Leave this alone... assumes /gizmo folder is in the same folder as this file.
 	require dirname(__FILE__).'/gizmo/FS.class.php';
