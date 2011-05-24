6fa42a23405e98a996873f26329bac93bb41b0e5
Alexander Whillas <whillas@gmail.com>
2011-05-24 20:52:35 +0200

    Moved LESS.php stuff and added plugin to handle precence of those files. Need to make a nicer plugin system for template files :-/

--- gizmo/includes/less.php/less.php
+++ gizmo/includes/less.php/less.php
@@ -1,8 +1,14 @@
 <?php
-	require_once dirname(__FILE__) . 'entities.less.class.php';
+	header('Content-type: text/css');
+
+	require_once dirname(__FILE__) . '/lib/entities.less.class.php';
 
 	$less = new LessCode();
 //	$less->setVariable('panes.input.select.hover.bg', 'XXXXXXXXXXXXXX');
 //	$less->parseFile($_SERVER['argv'][1]);
-	$less->parseFile($_GET['file']);
+	
+	$file = $_GET['file'];
+
+	$less->parseFile($file);
+	
 	echo $less->output();
---
+++ gizmo/plugins/less.class.php
@@ -0,0 +1,22 @@
+<?php
+require_once 'includes/html.php';
+
+/**
+* less.php plugin for server side processing of .less files
+* 
+* @link https://github.com/dresende/less.php
+*/
+class LESS
+{	
+	function __construct()
+	{
+		# code...
+	}
+	
+	public static function htmlLinkUrl($less_file_url)
+	{
+		$p = Path::open(INCLUDES_PATH.'/less.php/less.php');
+		
+		return $p->realURL().'?file='.$less_file_url;
+	}
+}
