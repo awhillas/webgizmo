5b63fe75c4120465dc90519344ee28fc55780e9f
Alexander Whillas <whillas@gmail.com>
2012-05-07 21:18:44 +0200

    Cleaned up formating

--- index.php
+++ index.php
@@ -7,14 +7,20 @@
 	 * @license http://www.gnu.org/copyleft/lesser.html LGPL
 	 **/
 
-	// Change these variables per website
-	define('SITE_TITLE', 	    'Website title here');	// Name of the website.
-	define('THEME_DIR',       '/yui');		// Template to use. Name of one of the folders in: /templates/html
-	define('FOLDER_DISPLAY',  'none');	// How subfolders are handled. Can be 'links', 'teaser' or 'none'
-	
-	define('DEBUG', false);	// set to True to see errors.
 
-	// Leave this alone... assumes /gizmo folder is in the same folder as this file.
+	// Change these variables per website...
+
+	// Name of the website.
+	define('SITE_TITLE', 'Website title here');	
+	
+	// Template to use. Name of one of the folders in: /templates/html
+	define('THEME_DIR', '/yui');
+
+
+
+	//  - - - - - - - - - - - - leave this part alone...
+
+	//  assumes /gizmo folder is in the same folder as this file.
 	require dirname(__FILE__).'/gizmo/FS.class.php';
 	
 	echo FS::get()->http();
