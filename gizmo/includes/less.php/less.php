e2fbd69f90c9d7af8c219793b255ab753882d979
Alexander Whillas <whillas@gmail.com>
2012-05-07 17:14:48 +0200

    Less support, changed less php support to https://github.com/leafo/lessphp

--- gizmo/includes/less.php/less.php
+++ gizmo/includes/less.php/less.php
@@ -1,14 +1,7 @@
 <?php
 	header('Content-type: text/css');
 
-	require_once dirname(__FILE__) . '/lib/entities.less.class.php';
-
-	$less = new LessCode();
-//	$less->setVariable('panes.input.select.hover.bg', 'XXXXXXXXXXXXXX');
-//	$less->parseFile($_SERVER['argv'][1]);
+	include dirname(__FILE__).'/lessc.inc.php'
 	
-	$file = $_GET['file'];
-
-	$less->parseFile($file);
-	
-	echo $less->output();
+	$less = new lessc($_GET['file']);
+	echo $less->parse();
