9be89a658b2d5e5d3c8e39b220ab92cb8a57d318
Alexander Whillas <whillas@gmail.com>
2012-10-16 18:07:18 +0200

    Updated the Galleria plugin

--- gizmo/plugins/galleria.class.php
+++ gizmo/plugins/galleria.class.php
@@ -12,60 +12,61 @@
  * 
  * Supports themes and has a full screen mode.
  * 
- * @link http://galleria.aino.se/
+ * @link http://galleria.io/
  * 
  * @package WebGizmo
  * @subpackage	GizPlugins
  * @author Alexander R B Whillas
  */
-class Galleria extends FSDir
+class Galleria extends GizDirPlugin
 {
 	
 	function html($format = 'html')
 	{
 		$id = $this->getID();
-		
 		$fs = FS::get();
-		$plugins = $fs->pluginsRoot()->realURL();
 		
+		// CSS
+		$fs->addRef(PLUGINS_URL.'/galleria/themes/classic/galleria.classic.css');
+		
+		// Javascript
 		$fs->addRef(JQUERY_URL);	// Include the CLI version of JQuery.
-		
-		$fs->addRef($plugins.'/galleria/galleria-1.2.3.min.js');
-		$fs->addRef($plugins.'/galleria/themes/classic/galleria.classic.min.js');
-	
-		$fs->add('
-			<script type="text/javascript">
-				$(document).ready(function() { 
-					$("#'.$id.'").galleria({
-						image_crop: true,
-						transition: "fade"
-				    }); 
-				});
-			</script>
-		');
+		$fs->addRef(PLUGINS_URL.'/galleria/galleria-1.2.8.min.js');
+		$fs->addRef(PLUGINS_URL.'/galleria/themes/classic/galleria.classic.min.js');
+		$fs->add('<script type="text/javascript">$(document).ready(function() { Galleria.run("#'.$id.'") }) </script>');
 		
 		$out = '';
-
 		// Assume the first folder in this folder is full of thumbnails (if it exists).
-		$thumbs = $this->getPath()->folders()->first()->images();
-		if($thumbs->tally() > 0) // are there any thumbnails?
+		$hasThumbnails = false;
+		if($thumbs = $this->getPath()->folders()->first())
 		{
-			$bigImgs = $this->images();
-			
-			foreach($thumbs as $small)
-				foreach($bigImgs as $large)
+			$thumbs = $thumbs->images();
+			$hasThumbnails = $thumbs->tally() > 0;
+		}
+		$bigImgs = $this->images();
+		$maxWidth = $maxHeight = 0;
+		foreach($bigImgs as $large)
+		{
+			$maxWidth = ($maxWidth < $large->width)? $large->width: $maxWidth;
+			$maxHeight = ($maxHeight < $large->height)? $large->height: $maxHeight;
+			if($hasThumbnails)
+			{
+				foreach($thumbs as $small)
+				{
 					if($small->getBasename() == $large->getBasename())
 					{
-						$out .= a($large->realURL(), $small->html());
+						$out .= a($large->getFileURL(), $small->html());
 						break;
 					}
-		} 
-		else
-		{
-			// Just dump all the images in the folder.
-			foreach($this->images() as $img)
-				$out .= $img->html()."\n";
+				}
+			}
+			else
+			{
+				$out .= $large->html()."\n";				
+			}
+			
 		}
+		$fs->add('<style>#'.$id.'{ width: '.$maxWidth.'px; height: '.$maxHeight.'px; }</style>');
 
 		return div($out, 'Galleria', $id);
 	}
