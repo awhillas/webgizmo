9d74575d50d2efa563ed958d869fab4fa940d561
Alexander Whillas <whillas@gmail.com>
2012-11-20 23:09:53 +0100

    UPdate to the Google Analytics include code. TODO: move to seperate include file

--- templates/html/giz/index.tpl.php
+++ templates/html/giz/index.tpl.php
@@ -70,19 +70,17 @@
 
 	</div>
 
-	<script type="text/javascript">
-
-	  var _gaq = _gaq || [];
-	  _gaq.push(['_setAccount', 'UA-1058558-9']);
-	  _gaq.push(['_trackPageview']);
-
-	  (function() {
-	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	  })();
-
-	</script>
-
+	<?php if(defined('GA_ID')): ?>
+		<script type="text/javascript">
+			var _gaq = _gaq || [];
+			_gaq.push(['_setAccount', '<?php echo GA_ID ?>']);
+			_gaq.push(['_trackPageview']);
+			(function() {
+				var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+				ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+				var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+			})();
+		</script>
+	<?php endif ?>
 </body>
 </html>
--- templates/html/yui/index.tpl.php
+++ templates/html/yui/index.tpl.php
@@ -16,7 +16,7 @@
 </head>
 <body class="<?php echo $fs->pathCSS() ?>">
 	
-	<div id="doc" class="yui-t1">
+	<div id="doc3" class="yui-t3">
 	
 		<div id="hd" role="banner">
 			<h1><a href="<?php echo $home ?>"><?php echo $title ?></a></h1>
@@ -50,13 +50,16 @@
 	</div>
 
 	<?php if(defined('GA_ID')): ?>
-		<script>
-		  var _gaq=[['_setAccount','<?php echo GA_ID ?>'],['_trackPageview']];
-		  (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-		  g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-		  s.parentNode.insertBefore(g,s)}(document,'script'));
+		<script type="text/javascript">
+			var _gaq = _gaq || [];
+			_gaq.push(['_setAccount', '<?php echo GA_ID ?>']);
+			_gaq.push(['_trackPageview']);
+			(function() {
+				var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+				ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+				var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+			})();
 		</script>
 	<?php endif ?>
-
 </body>
 </html>
